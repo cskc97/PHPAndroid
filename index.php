4d22a9874d2be1037091bdea32446143ff0d999e
cskc97 <cskc97@users.noreply.github.com>
2017-01-22 21:16:58 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,14 @@
 <?php
 
+require_once('include/database_functions.php');
+
+$data = new DatabaseFunctions();
+
+$username = "NewUser";
+$password = "NewUserPassword";
+
+$data->insertUser($username,$password);
+
 echo "Hello World";
 
 ?>
